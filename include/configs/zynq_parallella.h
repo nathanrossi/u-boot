45db953e5d71070dfaeb45f423103e9c4b1898d7
Nathan Rossi <nathan@nathanrossi.com>
2016-03-11 20:52:05 +1000

    zynq: Add Adapteva Parallella board support
    The Parallella is a single board computer showcasing the Epiphany
    multi-core processor from Adapteva. The board uses a Zynq-7000 SoC which
    interconnects to the Epiphany. This patch adds support for booting on
    the Zynq system of the board.
    
    More information on this board is available at: https://parallella.org/
    
    Signed-off-by: Nathan Rossi <nathan@nathanrossi.com>
    Cc: Albert Aribaud <albert.u.boot@aribaud.net>
    Cc: Michal Simek <monstr@monstr.eu>

---
+++ include/configs/zynq_parallella.h
@@ -0,0 +1,17 @@
+/*
+ * (C) Copyright 2016 Nathan Rossi
+ *
+ * Configuration for Adapteva Parallella Board
+ * See zynq-common.h for Zynq common configs
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQ_PARALLELLA_H
+#define __CONFIG_ZYNQ_PARALLELLA_H
+
+#define CONFIG_SYS_NO_FLASH
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_ZYNQ_PARALLELLA_H */
